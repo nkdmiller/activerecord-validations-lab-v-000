8ad208ebefb0bf819ea9cacaa2bdb926b726b46e
Nate Miller <natekmiller96@gmail.com>
2018-09-06 22:12:26 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,16 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
+  validate :clickbaity?
+
+  KEYWORDS = [
+    "Won't Believe", "Secret", "Top", "Guess"
+  ]
+  def clickbaity?
+      if title && KEYWORDS.none? {|headline| title.include?(headline)}
+        errors.add(:title, "not alluring enough")
+      end
+  end
 end
